76d73ab7913903f544897df22926d4f6c5f7e5de
DavidEguaras <dabenegu@gmail.com>
2023-10-09 19:06:47 +0200

    tarea07

---
+++ tareas/tarea007 copy/js/index.js
@@ -0,0 +1,27 @@
+//Declaramos las constantes de los inputs
+const filas = document.getElementById('filas');
+const caracterFilas = document.getElementById('caracterFilas');
+const relleno = document.getElementById('relleno');
+const colorElegido = document.getElementById('colorElegido');
+
+
+
+function calcularCuadradoHueco(){
+    
+}
+
+
+function calcularCuadradoRelleno(){
+
+}
+
+
+function calcularRomboHueco(){
+
+}
+
+
+function calcularRomboRelleno(){
+
+}
+
