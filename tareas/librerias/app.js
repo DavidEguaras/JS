764b74608f8ff70f37cbb5698f343af0085a85bc
DavidEguaras <dabenegu@gmail.com>
2023-11-02 18:24:35 +0100

    masCambios

--- tareas/librerias/app.js
+++ tareas/librerias/app.js
@@ -1,3 +1,12 @@
 import { funcionesMatematicas } from "./js/librerias.js";
 console.log(funcionesMatematicas.doble(10));
 console.log(funcionesMatematicas.triple(10));
+console.log(document.childNodes)
+const nodosH = document.getElementsByTagName('H1');
+console.log(nodosH);
+console.log(nodosH[0]);
+const nodosP = nodosH[0].getElementsByTagName('P')
+
+console.log(nodosP);
+console.log(nodosP[0]);
+
