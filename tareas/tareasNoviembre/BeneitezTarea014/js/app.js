737081096ba066d674ed56c04e5079eed62733d1
DavidEguaras <dabenegu@gmail.com>
2023-11-15 11:36:51 +0100

    tareas

---
+++ tareas/tareasNoviembre/BeneitezTarea014/js/app.js
@@ -0,0 +1,8 @@
+function cambiarColor(element){
+    // element.style.backgroundColor = 'rojo';
+    if (element.style.backgroundColor == 'red'){
+        element.style.backgroundColor = 'white';
+    }else{
+        element.style.backgroundColor = 'red';
+    }
+}   
--- tareas/tareasNoviembre/BeneitezTarea016/js/app.js
+++ tareas/tareasNoviembre/BeneitezTarea016/js/app.js
@@ -2,8 +2,8 @@
 import { Bola } from "./bola.js";
 
 const bola1 = new Bola(85, 100, 100, 'red', 'linear-gradient(to bottom, #2196f3, #009688)');
-const bola3 = new Bola(55, 500, 300, '#f09688');
-const bola4 = new Bola(60, 700, 400, 'green');
+const bola3 = new Bola(55, 200, 300, '#f09688');
+const bola4 = new Bola(60, 300, 400, 'green');
 
 
 Bola.arrayBolas.forEach(bola => bola.verBola());
