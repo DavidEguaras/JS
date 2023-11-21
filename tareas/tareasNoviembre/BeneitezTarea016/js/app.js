e58b9741d4e802ce83c9a7aca114a3a5192e3e76
DavidEguaras <dabenegu@gmail.com>
2023-11-21 16:01:25 +0100

    commit

--- tareas/tareasNoviembre/BeneitezTarea016/js/app.js
+++ tareas/tareasNoviembre/BeneitezTarea016/js/app.js
@@ -1,6 +1,7 @@
 
 import { Bola } from "./bola.js";
 
+
 const bola1 = new Bola(85, 100, 100, 'red', 'linear-gradient(to bottom, #2196f3, #009688)');
 const bola3 = new Bola(55, 200, 300, '#f09688');
 const bola4 = new Bola(60, 300, 400, 'green');
